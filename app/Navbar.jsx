c24a2dd44e11db2fe8cda3266fd7a95f1044399d
rahul <krahul8197@gmail.com>
2025-09-02 08:13:26 +0530

    admin page

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-900 py-4 px-6 flex justify-between items-center shadow-md">
       <h1 className="text-2xl font-bold text-white tracking-wide font-sans">School Search</h1>
-      <div>
+      <div className=" flex gap-4 mx-2">
         {pathname === "/show-school" ? (
           <Link href="/">
             <button className="bg-green-600 hover:bg-green-700 text-white font-semibold px-5 py-2 rounded transition-all duration-200">Home</button>
@@ -17,6 +17,10 @@ export default function Navbar() {
             <button className="bg-green-600 hover:bg-green-700 text-white font-semibold px-5 py-2 rounded transition-all duration-200">Show All</button>
           </Link>
         ) : null}
+
+        <Link href="/admin">
+            <button className="bg-green-600 hover:bg-green-700 text-white font-semibold px-5 py-2 rounded transition-all duration-200  ">Admin</button>
+          </Link>
       </div>
     </nav>
   );
